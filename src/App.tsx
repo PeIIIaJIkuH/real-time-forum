89f8e5caf71d6e32f5cadd7a1029f717239027e5
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-05 08:12:07 +0600

    update: added main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,57 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import {IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from '@ionic/react'
+import {IonReactRouter} from '@ionic/react-router'
+import '@ionic/react/css/core.css'
+import '@ionic/react/css/display.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/float-elements.css'
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import '@ionic/react/css/normalize.css'
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/typography.css'
+import {chatbubbleEllipsesOutline, personOutline, readerOutline} from 'ionicons/icons'
+import {FC} from 'react'
+import {Redirect, Route, Switch} from 'react-router'
+import {Chats} from './pages/Chats'
+import {Posts} from './pages/Posts'
+import {Profile} from './pages/Profile'
 
 /* Theme variables */
-import './theme/variables.css';
+import './theme/variables.css'
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
-
-export default App;
+export const App: FC = () => (
+	<IonApp>
+		<IonReactRouter>
+			<IonTabs>
+				<IonRouterOutlet>
+					<Switch>
+						<Route exact path='/posts'><Posts/></Route>
+						<Route exact path='/chats'><Chats/></Route>
+						<Route exact path='/profile'><Profile/></Route>
+						<Redirect exact path='/' to='/posts'/>
+					</Switch>
+				</IonRouterOutlet>
+				<IonTabBar slot='bottom'>
+					<IonTabButton tab='posts' href='/posts'>
+						<IonIcon icon={readerOutline}/>
+						<IonLabel>Posts</IonLabel>
+					</IonTabButton>
+					<IonTabButton tab='chats' href='/chats'>
+						<IonIcon icon={chatbubbleEllipsesOutline}/>
+						<IonLabel>Chats</IonLabel>
+					</IonTabButton>
+					<IonTabButton tab='profile' href='/profile'>
+						<IonIcon icon={personOutline}/>
+						<IonLabel>Profile</IonLabel>
+					</IonTabButton>
+				</IonTabBar>
+			</IonTabs>
+		</IonReactRouter>
+	</IonApp>
+)
