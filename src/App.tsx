8a2ba7d3480c2be4bb2b54c6749f48241707ceea
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-07 13:55:35 +0600

    update: added change between profile and auth tabs

--- src/App.tsx
+++ src/App.tsx
@@ -15,18 +15,20 @@ import '@ionic/react/css/structure.css'
 import '@ionic/react/css/text-alignment.css'
 import '@ionic/react/css/text-transformation.css'
 import '@ionic/react/css/typography.css'
-import {chatbubbleEllipsesOutline, personOutline, readerOutline} from 'ionicons/icons'
+import {chatbubbleEllipsesOutline, logInOutline, personOutline, readerOutline} from 'ionicons/icons'
+import {observer} from 'mobx-react-lite'
 import {FC} from 'react'
 import {Redirect, Route, Switch} from 'react-router'
 import {Auth} from './pages/Auth/Auth'
 import {Chats} from './pages/Chats'
 import {Posts} from './pages/Posts'
 import {Profile} from './pages/Profile'
+import authState from './store/authState'
 
 /* Theme variables */
 import './theme/variables.css'
 
-export const App: FC = () => (
+export const App: FC = observer(() => (
 	<IonApp>
 		<IonReactRouter>
 			<IonTabs>
@@ -48,12 +50,19 @@ export const App: FC = () => (
 						<IonIcon icon={chatbubbleEllipsesOutline}/>
 						<IonLabel>Chats</IonLabel>
 					</IonTabButton>
-					<IonTabButton tab='profile' href='/profile'>
-						<IonIcon icon={personOutline}/>
-						<IonLabel>Profile</IonLabel>
-					</IonTabButton>
+					{authState.isAuth ? (
+						<IonTabButton tab='profile' href='/profile'>
+							<IonIcon icon={personOutline}/>
+							<IonLabel>Profile</IonLabel>
+						</IonTabButton>
+					) : (
+						<IonTabButton tab='auth' href='/auth'>
+							<IonIcon icon={logInOutline}/>
+							<IonLabel>Auth</IonLabel>
+						</IonTabButton>
+					)}
 				</IonTabBar>
 			</IonTabs>
 		</IonReactRouter>
 	</IonApp>
-)
+))
