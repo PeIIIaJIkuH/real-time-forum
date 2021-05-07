fc9e9e6d7519b70de87a6f70f70ab64cfabdaf4d
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-07 12:52:40 +0600

    update: added validation schemas for register and login

---
+++ src/utils/validationSchemas.ts
@@ -0,0 +1,64 @@
+import * as yup from 'yup'
+
+export const registerValidationSchema = yup.object({
+	username: yup
+		.string()
+		.nullable()
+		.min(2, 'Minimum 2 characters')
+		.max(20, 'Maximum 20 characters')
+		.matches(/^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/, 'Only alphanumeric characters with [" ", "_", "-"]')
+		.required('Required'),
+	email: yup
+		.string()
+		.nullable()
+		.email('Invalid email')
+		.required('Required'),
+	password: yup
+		.string()
+		.nullable()
+		.min(6, 'Minimum 6 characters')
+		.max(20, 'Maximum 20 characters')
+		.required('Required'),
+	rePassword: yup
+		.string()
+		.nullable()
+		.oneOf([yup.ref('password')], 'Passwords must match')
+		.required('Required'),
+	firstName: yup
+		.string()
+		.nullable()
+		.min(2, 'Minimum 2 characters')
+		.max(20, 'Maximum 20 characters')
+		.matches(/^[A-Za-z]+$/, 'Only alphabetic characters')
+		.required('Required'),
+	lastName: yup
+		.string()
+		.nullable()
+		.min(2, 'Minimum 2 characters')
+		.max(20, 'Maximum 20 characters')
+		.matches(/^[A-Za-z]+$/, 'Only alphabetic characters')
+		.required('Required'),
+	age: yup
+		.number()
+		.nullable()
+		.positive('Only positive numbers')
+		.integer('Only integers')
+		.min(0, 'Minimum 0')
+		.max(100, 'Maximum 100')
+		.required('Required'),
+	gender: yup
+		.string()
+		.nullable()
+		.required('Required')
+})
+
+export const loginValidationSchema = yup.object({
+	usernameOrEmail: yup
+		.string()
+		.nullable()
+		.required('Required'),
+	password: yup
+		.string()
+		.nullable()
+		.required('Required')
+})
