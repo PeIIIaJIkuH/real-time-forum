c9ca5c8dee783b30b612b2552b17750811dc2e3f
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-09 05:12:10 +0600

    update: added sign out button

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,26 @@
-import {IonContent, IonFooter, IonHeader, IonMenu, IonTitle, IonToolbar} from '@ionic/react'
-import {FC} from 'react'
+import {IonButton, IonContent, IonFooter, IonHeader, IonMenu, IonTitle, IonToolbar} from '@ionic/react'
+import {observer} from 'mobx-react-lite'
+import {FC, useRef} from 'react'
+import authState from '../store/authState'
 
-export const Menu: FC = () => {
+export const Menu: FC = observer(() => {
+	const menuRef = useRef<HTMLIonMenuElement>(null)
+
 	return (
-		<IonMenu side='start' contentId='main'>
+		<IonMenu contentId='main' ref={menuRef}>
 			<IonHeader>
 				<IonToolbar>
 					<IonTitle>Real time forum</IonTitle>
 				</IonToolbar>
 			</IonHeader>
 			<IonContent>
+				{authState.user && (
+					<IonButton onClick={async () => {
+						await authState.signOut()
+						await menuRef.current!.close()
+					}
+					}>Log out</IonButton>
+				)}
 			</IonContent>
 			<IonFooter>
 				<IonToolbar>
@@ -18,4 +29,4 @@ export const Menu: FC = () => {
 			</IonFooter>
 		</IonMenu>
 	)
-}
+})
