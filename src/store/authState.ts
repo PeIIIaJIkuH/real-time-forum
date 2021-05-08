31658e5c027d0c98cb6744531bc56c252e97479c
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-09 05:11:37 +0600

    update: added fetchUserData and signOut methods

--- src/store/authState.ts
+++ src/store/authState.ts
@@ -1,20 +1,37 @@
 import {makeAutoObservable} from 'mobx'
+import {authAPI} from '../api/auth'
 import {IUser} from '../types'
 
 class AuthState {
-	isAuth: boolean = false
 	user: IUser | null = null
 
 	constructor() {
 		makeAutoObservable(this)
 	}
 
-	setIsAuth(isAuth: boolean) {
-		this.isAuth = isAuth
-	}
-	
-	setUser(user: IUser) {
+	setUser(user: IUser | null) {
 		this.user = user
+	}
+
+	async fetchUserData() {
+		const response = await authAPI.me()
+		if (response.state) {
+			const {nickname, email, firstName, lastName, age, gender, createdAt, lastActive} = response.data
+			this.setUser({
+				username: nickname, email, firstName, lastName, age, gender, createdAt, lastActive
+			})
+		} else {
+
+		}
+	}
+
+	async signOut() {
+		const response = await authAPI.signOut()
+		if (response.state) {
+			this.setUser(null)
+		} else {
+
+		}
 	}
 }
 
