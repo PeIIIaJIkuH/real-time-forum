d488e7d12ae17f1d49eeb2cc69767203cc7d9380
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-09 05:12:47 +0600

    update: added signIn, signOut, and me functions

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,4 @@
-import {ApiResponse, RegisterValues} from '../types'
+import {ApiResponse, LoginValues, RegisterValues} from '../types'
 import {defaultAxios} from './index'
 
 const signUp = (values: RegisterValues): Promise<ApiResponse> => defaultAxios.post('user/signup', {
@@ -6,6 +6,20 @@ const signUp = (values: RegisterValues): Promise<ApiResponse> => defaultAxios.po
 	nickname: values.username
 }).then(r => r.data).catch(e => e.response.data)
 
+const signIn = (values: LoginValues): Promise<ApiResponse> => defaultAxios.post('session/login', {
+	nickname: values.usernameOrEmail,
+	password: values.password
+}).then(r => r.data).catch(e => e.response.data)
+
+const signOut = (): Promise<ApiResponse> => defaultAxios.delete('session/logout')
+	.then(r => r.data).catch(e => e.response.data)
+
+const me = (): Promise<ApiResponse> => defaultAxios.get('user/me')
+	.then(r => r.data).catch(e => e.response.data)
+
 export const authAPI = {
-	signUp
+	signUp,
+	signIn,
+	signOut,
+	me
 }
