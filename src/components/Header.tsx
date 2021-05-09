eacebea783154d8c40416c969b3fc48f4392a991
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-09 14:26:36 +0600

    refactor: changed titel logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import {IonButtons, IonHeader, IonMenuButton, IonTitle, IonToolbar} from '@ionic/react'
 import {observer} from 'mobx-react-lite'
 import {FC} from 'react'
-import appState from '../store/appState'
 
-export const Header: FC = observer(() => {
+interface Props {
+	title: string
+}
+
+export const Header: FC<Props> = observer(({title}) => {
 	return (
 		<IonHeader translucent>
 			<IonToolbar>
+				<IonTitle>{title}</IonTitle>
 				<IonButtons slot='end'>
 					<IonMenuButton/>
 				</IonButtons>
-				<IonTitle>{appState.title}</IonTitle>
 			</IonToolbar>
 		</IonHeader>
 	)
