46776727aa2000a258d1668d733a5115119d5541
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-09 20:12:02 +0600

    update: added appState

---
+++ src/store/appState.ts
@@ -0,0 +1,15 @@
+import {makeAutoObservable} from 'mobx'
+
+class AppState {
+	isLoading: boolean = false
+	
+	constructor() {
+		makeAutoObservable(this)
+	}
+	
+	setIsLoading(isLoading: boolean) {
+		this.isLoading = isLoading
+	}
+}
+
+export default new AppState()
