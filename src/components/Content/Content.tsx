d6347abf0bd9626d752153194de2e130eb8e25a1
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-09 20:11:15 +0600

    update: added Content wrapper

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,15 @@
+import {IonContent, IonProgressBar} from '@ionic/react'
+import clsx from 'clsx'
+import {observer} from 'mobx-react-lite'
+import {FC} from 'react'
+import appState from '../../store/appState'
+import s from './Content.module.css'
+
+export const Content: FC = observer(({children}) => {
+	return (
+		<IonContent>
+			<IonProgressBar type='indeterminate' className={clsx(s.progressBar, appState.isLoading && s.show)}/>
+			{children}
+		</IonContent>
+	)
+})
