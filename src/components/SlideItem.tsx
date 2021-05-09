12723239368de776c7c066e6dae5546834ffe99d
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-09 14:27:07 +0600

    refactor: included IonPage

--- src/components/SlideItem.tsx
+++ src/components/SlideItem.tsx
@@ -1,18 +1,14 @@
-import {IonCol, IonContent, IonGrid, IonPage, IonRow} from '@ionic/react'
+import {IonCol, IonGrid, IonRow} from '@ionic/react'
 import {FC} from 'react'
 
 export const SlideItem: FC = ({children}) => {
 	return (
-		<IonPage>
-			<IonContent>
-				<IonGrid fixed className='ion-no-padding'>
-					<IonRow>
-						<IonCol size='12' sizeSm='8' offsetSm='2' sizeMd='6' offsetMd='3' sizeLg='4' offsetLg='4' sizeXl='4' offsetXl='4'>
-							{children}
-						</IonCol>
-					</IonRow>
-				</IonGrid>
-			</IonContent>
-		</IonPage>
+		<IonGrid fixed className='ion-no-padding ion-align-self-start'>
+			<IonRow>
+				<IonCol size='12' sizeSm='8' offsetSm='2' sizeMd='6' offsetMd='3' sizeLg='4' offsetLg='4' sizeXl='4' offsetXl='4'>
+					{children}
+				</IonCol>
+			</IonRow>
+		</IonGrid>
 	)
 }
