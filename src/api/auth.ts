ff61f6393f3bedcad2c609950dcfc02e34d4cc85
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-13 23:54:14 +0600

    refactor: shortened code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,21 +1,14 @@
 import {ApiResponse, LoginValues, RegisterValues} from '../types'
 import {defaultAxios} from './index'
 
-const signUp = (values: RegisterValues): Promise<ApiResponse> => defaultAxios.post('user/signup', {
-	...values,
-	nickname: values.username
-}).then(r => r.data).catch(e => e.response.data)
+const signUp = (values: RegisterValues): Promise<ApiResponse> =>
+	defaultAxios.post('user/signup', values)
 
-const signIn = (values: LoginValues): Promise<ApiResponse> => defaultAxios.post('session/login', {
-	nickname: values.usernameOrEmail,
-	password: values.password
-}).then(r => r.data).catch(e => e.response.data)
+const signIn = (values: LoginValues): Promise<ApiResponse> => defaultAxios.post('session/login', values)
 
 const signOut = (): Promise<ApiResponse> => defaultAxios.delete('session/logout')
-	.then(r => r.data).catch(e => e.response.data)
 
-const me = (): Promise<ApiResponse> => defaultAxios.get('user/me')
-	.then(r => r.data).catch(e => e.response.data)
+const me = (): Promise<ApiResponse> => defaultAxios.get('users/me')
 
 export const authAPI = {
 	signUp,
