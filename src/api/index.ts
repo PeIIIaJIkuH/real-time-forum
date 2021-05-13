b4c5333154df0038e928531f7eb419aff332bd4c
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-05-13 23:53:55 +0600

    update: added interceptor to axios response

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,3 +4,5 @@ export const defaultAxios = axios.create({
 	baseURL: 'http://localhost:8081/api/v1/',
 	withCredentials: true
 })
+
+defaultAxios.interceptors.response.use(r => r.data, e => e.response.data)
